851a2a8a20e3a818e71db135cb649043a521b8e5
Yang.Zhang9 <Yang.Zhang9@homecredit.cn>
2020-04-24 15:11:35 +0800

    .................

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,17 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  */
+@RestController
+@SpringBootApplication
 public class App
 {
 
@@ -12,10 +21,16 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        SpringApplication.run(App.class, args);
     }
 
     private final String getMessage() {
         return message;
+    }
+
+    @RequestMapping(value = "/version", method = RequestMethod.GET)
+    public @ResponseBody String vers() {
+        return "1.0";
     }
 
 }
